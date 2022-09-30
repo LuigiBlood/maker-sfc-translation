3c39566aa46d41bfd3d41ae27aa683a80ecfa0d0
LuigiBlood <luigiblood@gmail.com>
2022-10-01 00:44:46 +0200

    [Ongaku] Translate New Score Reset prompt

--- OngakuTsukuru/_0decomp_jp.bat
+++ OngakuTsukuru/_0decomp_jp.bat
@@ -4,3 +4,4 @@
 .\tools\decompress.py .\roms\OngakuTsukuruJP.sfc 0x1930 .\gfx\jp\score_topmenu_bar.chr
 .\tools\decompress.py .\roms\OngakuTsukuruJP.sfc 0x8CA60 .\gfx\jp\score_ui.chr
 .\tools\decompress.py .\roms\OngakuTsukuruJP.sfc 0x36B88 .\gfx\jp\score_topmenu_score.chr
+.\tools\decompress.py .\roms\OngakuTsukuruJP.sfc 0xBAC10 .\gfx\jp\score_new_prompt.chr
--- OngakuTsukuru/_1comp_en.bat
+++ OngakuTsukuru/_1comp_en.bat
@@ -2,4 +2,5 @@
 .\tools\compress.py .\gfx\en\score_topmenu_bar.chr .\gfx\en\tmp\score_topmenu_bar.chr.cmp
 .\tools\compress.py .\gfx\en\score_topmenu_score.chr .\gfx\en\tmp\score_topmenu_score.chr.cmp
 .\tools\compress.py .\gfx\en\score_topmenu_data.chr .\gfx\en\tmp\score_topmenu_data.chr.cmp
+.\tools\compress.py .\gfx\en\score_new_prompt.chr .\gfx\en\tmp\score_new_prompt.chr.cmp
 .\tools\compress.py .\gfx\en\score_ui.chr .\gfx\en\tmp\score_ui.chr.cmp
