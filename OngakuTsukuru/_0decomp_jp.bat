bdaa1eb0d48ac895638dfcfa88fde4e77b76e669
LuigiBlood <luigiblood@gmail.com>
2022-10-01 16:01:35 +0200

    [Ongaku] Translate New Score Window

--- OngakuTsukuru/_0decomp_jp.bat
+++ OngakuTsukuru/_0decomp_jp.bat
@@ -5,3 +5,4 @@
 .\tools\decompress.py .\roms\OngakuTsukuruJP.sfc 0x8CA60 .\gfx\jp\score_ui.chr
 .\tools\decompress.py .\roms\OngakuTsukuruJP.sfc 0x36B88 .\gfx\jp\score_topmenu_score.chr
 .\tools\decompress.py .\roms\OngakuTsukuruJP.sfc 0xBAC10 .\gfx\jp\score_new_prompt.chr
+.\tools\decompress.py .\roms\OngakuTsukuruJP.sfc 0x3648 .\gfx\jp\score_new_window.chr
--- OngakuTsukuru/_1comp_en.bat
+++ OngakuTsukuru/_1comp_en.bat
@@ -3,4 +3,5 @@
 .\tools\compress.py .\gfx\en\score_topmenu_score.chr .\gfx\en\tmp\score_topmenu_score.chr.cmp
 .\tools\compress.py .\gfx\en\score_topmenu_data.chr .\gfx\en\tmp\score_topmenu_data.chr.cmp
 .\tools\compress.py .\gfx\en\score_new_prompt.chr .\gfx\en\tmp\score_new_prompt.chr.cmp
+.\tools\compress.py .\gfx\en\score_new_window.chr .\gfx\en\tmp\score_new_window.chr.cmp
 .\tools\compress.py .\gfx\en\score_ui.chr .\gfx\en\tmp\score_ui.chr.cmp
